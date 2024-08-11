fabde4c7e2b5b85a66d87e876e5849b97095662c
AlejandroPuentes <alejandropuentes@AsusLinux.168.1.20>
2024-08-11 13:52:09 -0400

    Version 1.1

--- consultas.py
+++ consultas.py
@@ -1,4 +1,5 @@
 import psycopg2
+from psycopg2 import sql
 
 #Datos de conexion
 
@@ -13,13 +14,13 @@ class Consultas:
         self.user = user
         self.password = password
         self.data_base = data_base
-
+        self.con = None
 
 
     def connect_libreria(self):
 
         try:
-            connection = psycopg2.connect(
+            self.con = psycopg2.connect(
                 host = "localhost",
                 user = "postgres",
                 password = "0711white",
@@ -32,6 +33,51 @@ class Consultas:
 
             print("Error, connection failed")
 
+        cur = self.con.cursor()
+
+
+
+    #Asi podemos usar cur dentro de toda la clase
+    def obtener_cursor(self):
+        
+        if not self.con:
+            print("NO hay conexion")
+
+        return self.con.cursor()
+    
+
+
+
+    def view_data(self,n_tabla):
+
+        cur = self.obtener_cursor()
+
+        consulta_sql = sql.SQL("SELECT * FROM {table};"). format(
+            table= sql.Identifier(n_tabla)
+        )
+
+        try:
+            cur.execute(consulta_sql)
+
+            for result in cur.fetchall():
+
+                print(result)
+            
+            cur.close()
+
+
+        except psycopg2.Error as e:
+
+            print("Querry unsuccesful")
+
+
+
+        
+
+
+
+
+
 
 
 def main():
@@ -41,6 +87,7 @@ def main():
 
     #Ejecutamos
     iniciar.connect_libreria()
+    iniciar.view_data("categorias")
 
 
 if __name__ == "__main__":
